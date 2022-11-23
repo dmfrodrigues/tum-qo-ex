b4dcd81b54aadd8108fd9c954e04c68165030a09
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-11-23 21:25:12 +0100

    Few more changes to plan

--- src/Plan.hpp
+++ src/Plan.hpp
@@ -7,12 +7,21 @@ struct Plan {
    int id = -1;
    double cardinality = 0.0;
    double cost = 0.0;
-   Plan(int id_): id(id_){}
-   Plan(Plan *left_, Plan *right_): left(left_), right(right_){}
+   explicit Plan(int id, double cardinality): id(id), cardinality(cardinality){}
+   explicit Plan(Plan *left, Plan *right, double selectivity):
+      left(left),
+      right(right),
+      cardinality(left->cardinality * right->cardinality * selectivity)
+      {}
    auto operator<(const Plan& p) const { return cost < p.cost; }
    ~Plan(){
       delete left;
       delete right;
+   }
+
+   uint64_t getBitset() const {
+      if(left) return left->getBitset() | right->getBitset();
+      else return (uint64_t(1) << id);
    }
 };
 }
