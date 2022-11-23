3bdf86c087779ebc4c0bf1b0784f18bc880256b0
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2022-11-23 18:23:29 +0100

    Modified plan slightly

--- src/Plan.hpp
+++ src/Plan.hpp
@@ -7,6 +7,12 @@ struct Plan {
    int id = -1;
    double cardinality = 0.0;
    double cost = 0.0;
+   Plan(int id_): id(id_){}
+   Plan(Plan *left_, Plan *right_): left(left_), right(right_){}
    auto operator<(const Plan& p) const { return cost < p.cost; }
+   ~Plan(){
+      delete left;
+      delete right;
+   }
 };
 }
